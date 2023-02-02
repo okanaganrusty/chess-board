f916f7d10795bfbfa403e44a08570d7f73f91efc
Russell McConnachie <russell@mcconnachie.ca>
2023-02-01 19:44:52 -0800

    Chess game update.

--- src/App.js
+++ src/App.js
@@ -14,42 +14,42 @@ function App() {
   const [chessPieces, setChessPieces] = useState(() => {
     const initialPositions = new Array(64);
 
-    initialPositions[0] = <Rook />;
-    initialPositions[1] = <Knight />;
-    initialPositions[2] = <Bishop />;
-    initialPositions[3] = <King />;
-    initialPositions[4] = <Queen />;
-    initialPositions[5] = <Bishop />;
-    initialPositions[6] = <Knight />;
-    initialPositions[7] = <Rook />;
+    initialPositions[0] = <Rook x={0} y={0} />;
+    initialPositions[1] = <Knight x={1} y={0} />;
+    initialPositions[2] = <Bishop x={2} y={0} />;
+    initialPositions[3] = <King x={3} y={0} />;
+    initialPositions[4] = <Queen x={4} y={0} />;
+    initialPositions[5] = <Bishop x={5} y={0} />;
+    initialPositions[6] = <Knight x={6} y={0} />;
+    initialPositions[7] = <Rook x={7} y={0} />;
 
-    initialPositions[8] = <Pawn />;
-    initialPositions[9] = <Pawn />;
-    initialPositions[10] = <Pawn />;
-    initialPositions[11] = <Pawn />;
-    initialPositions[12] = <Pawn />;
-    initialPositions[13] = <Pawn />;
-    initialPositions[14] = <Pawn />;
-    initialPositions[15] = <Pawn />;
+    initialPositions[8] = <Pawn x={0} y={1} />;
+    initialPositions[9] = <Pawn x={1} y={1} />;
+    initialPositions[10] = <Pawn x={2} y={1} />;
+    initialPositions[11] = <Pawn x={3} y={1} />;
+    initialPositions[12] = <Pawn x={4} y={1} />;
+    initialPositions[13] = <Pawn x={5} y={1} />;
+    initialPositions[14] = <Pawn x={6} y={1} />;
+    initialPositions[15] = <Pawn x={7} y={1} />;
 
     // Black pieces
-    initialPositions[63] = <Rook isBlackPiece={true} />;
-    initialPositions[62] = <Knight isBlackPiece={true} />;
-    initialPositions[61] = <Bishop isBlackPiece={true} />;
-    initialPositions[60] = <King isBlackPiece={true} />;
-    initialPositions[59] = <Queen isBlackPiece={true} />;
-    initialPositions[58] = <Bishop isBlackPiece={true} />;
-    initialPositions[57] = <Knight isBlackPiece={true} />;
-    initialPositions[56] = <Rook isBlackPiece={true} />;
+    initialPositions[63] = <Rook x={0} y={7} isBlackPiece={true} />;
+    initialPositions[62] = <Knight x={1} y={7} isBlackPiece={true} />;
+    initialPositions[61] = <Bishop x={2} y={7} isBlackPiece={true} />;
+    initialPositions[60] = <King x={3} y={7} isBlackPiece={true} />;
+    initialPositions[59] = <Queen x={4} y={7} isBlackPiece={true} />;
+    initialPositions[58] = <Bishop x={5} y={7} isBlackPiece={true} />;
+    initialPositions[57] = <Knight x={6} y={7} isBlackPiece={true} />;
+    initialPositions[56] = <Rook x={7} y={7} isBlackPiece={true} />;
 
-    initialPositions[55] = <Pawn isBlackPiece={true} />;
-    initialPositions[54] = <Pawn isBlackPiece={true} />;
-    initialPositions[53] = <Pawn isBlackPiece={true} />;
-    initialPositions[52] = <Pawn isBlackPiece={true} />;
-    initialPositions[51] = <Pawn isBlackPiece={true} />;
-    initialPositions[50] = <Pawn isBlackPiece={true} />;
-    initialPositions[49] = <Pawn isBlackPiece={true} />;
-    initialPositions[48] = <Pawn isBlackPiece={true} />;
+    initialPositions[55] = <Pawn x={0} y={6} isBlackPiece={true} />;
+    initialPositions[54] = <Pawn x={1} y={6} isBlackPiece={true} />;
+    initialPositions[53] = <Pawn x={2} y={6} isBlackPiece={true} />;
+    initialPositions[52] = <Pawn x={3} y={6} isBlackPiece={true} />;
+    initialPositions[51] = <Pawn x={4} y={6} isBlackPiece={true} />;
+    initialPositions[50] = <Pawn x={5} y={6} isBlackPiece={true} />;
+    initialPositions[49] = <Pawn x={6} y={6} isBlackPiece={true} />;
+    initialPositions[48] = <Pawn x={7} y={6} isBlackPiece={true} />;
 
     return initialPositions;
   });
@@ -67,25 +67,34 @@ function App() {
     <div className="App">
       <div
         style={{
-          display: "grid",
-          minWidth: "120px",
-          minHeight: "120px",
-          width: "120px",
-          height: "120px",
-          gridGap: "2px",
-          gridTemplateColumns: "repeat(8, 1fr)",
-          gridTemplateRows: "repeat(8, 1fr)",
+          display: "flex",
+          flexDirection: "column"
         }}
       >
-        {[...Array(8)].map((e, row) =>
-          [...Array(8)].map((f, column) => (
-            <div key={(row * 8) + column} className="game-cell" style={{
-              backgroundColor: (row * 8 + column) % 2 === (row % 2) ? "#d0d0d0" : ""
-            }}>
-              {chessPieces[row * 8 + column]}
-            </div>  
-          ))
-        )}
+        {[...Array(8)].map((e, row) => (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              flexGrow: "1",
+            }}
+          >
+            {[...Array(8)].map((f, column) => (            
+              <div                
+                key={row * 8 + column}
+                className="game-cell"
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  backgroundColor:
+                    (row + column) % 2 ? "#d0d0d0" : "",
+                }}
+              >
+                {chessPieces[row * 8 + column]}
+              </div>
+            ))}
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/Pawn.js
+++ src/Pawn.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 
 import black from "./assets/black_pawn.png";
 import white from "./assets/white_pawn.png";
 
 const Pawn = (props) => {
-    const { isBlackPiece } = props;
+  const { isBlackPiece, x, y } = props;
+  const [xPosition, setXPosition] = useState(x);
+  const [yPosition, setYPosition] = useState(y);
 
-    return <><img src={isBlackPiece ? black : white} alt="" /></>;
-}
+  return (
+    <div>
+      <img src={isBlackPiece ? black : white} alt="" />
+      x: <pre>{xPosition}</pre>, y: <pre>{yPosition}</pre>
+    </div>
+  );
+};
 
 export default Pawn;
