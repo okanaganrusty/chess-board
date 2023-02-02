d8b2340cd79a22455a5707cc4b4a9a532cfcd0b4
Russell McConnachie <russell@mcconnachie.ca>
2023-02-01 18:29:55 -0800

    Update initial positions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useState, useEffect } from 'react';
+
 import King from './King';
 import Queen from './Queen';
 import Bishop from './Bishop';
@@ -9,6 +11,32 @@ import Rook from './Rook';
 import Pawn from './Pawn';
 
 function App() {
+  const [chessPieces, setChessPieces] = useState(new Array(64));
+
+  useEffect(() => {
+    setChessPieces((initialPositions) => {
+      initialPositions[0] = <Rook />
+      initialPositions[1] = <Knight />
+      initialPositions[2] = <Bishop />      
+      initialPositions[3] = <King />      
+      initialPositions[4] = <Queen />            
+      initialPositions[5] = <Bishop />      
+      initialPositions[6] = <Knight />      
+      initialPositions[7] = <Rook />      
+    
+      initialPositions[8] = <Pawn />
+      initialPositions[9] = <Pawn />
+      initialPositions[10] = <Pawn />
+      initialPositions[11] = <Pawn />
+      initialPositions[12] = <Pawn />
+      initialPositions[13] = <Pawn />
+      initialPositions[14] = <Pawn />
+      initialPositions[15] = <Pawn />
+
+      return initialPositions;
+    })
+  }, [])
+
   return (
     <div className="App">
 
@@ -19,82 +47,10 @@ function App() {
         width: "120px",
         height: "120px",
         gridGap: "2px",
-        border: "1px solid black",
         gridTemplateColumns: "repeat(8, 1fr)",
         gridTemplateRows: "repeat(8, 1fr)",
-       }}>        
-        <div className="game-cell"></div>
-        <div className="game-cell"></div>        
-        <div className="game-cell"></div>
-        <div className="game-cell"></div>        
-        <div className="game-cell"></div>
-        <div className="game-cell"></div>
-        <div className="game-cell"></div>
-        <div className="game-cell"></div>
-
-        <div className="game-cell"></div>
-        <div className="game-cell"></div>        
-        <div className="game-cell"></div>
-        <div className="game-cell"></div>        
-        <div className="game-cell"></div>
-        <div className="game-cell"></div>
-        <div className="game-cell"></div>
-        <div className="game-cell"></div>
-
-        <div className="game-cell"></div>
-        <div className="game-cell"></div>        
-        <div className="game-cell"></div>
-        <div className="game-cell"></div>        
-        <div className="game-cell"></div>
-        <div className="game-cell"></div>
-        <div className="game-cell"></div>
-        <div className="game-cell"></div>
-
-        <div className="game-cell"></div>
-        <div className="game-cell"></div>        
-        <div className="game-cell"></div>
-        <div className="game-cell"></div>        
-        <div className="game-cell"></div>
-        <div className="game-cell"></div>
-        <div className="game-cell"></div>
-        <div className="game-cell"></div>
-
-        <div className="game-cell"></div>
-        <div className="game-cell"></div>        
-        <div className="game-cell"></div>
-        <div className="game-cell"></div>        
-        <div className="game-cell"></div>
-        <div className="game-cell"></div>
-        <div className="game-cell"></div>
-        <div className="game-cell"></div>
-
-        <div className="game-cell"></div>
-        <div className="game-cell"></div>        
-        <div className="game-cell"></div>
-        <div className="game-cell"></div>        
-        <div className="game-cell"></div>
-        <div className="game-cell"></div>
-        <div className="game-cell"></div>
-        <div className="game-cell"></div>
-
-        <div className="game-cell"></div>
-        <div className="game-cell"></div>        
-        <div className="game-cell"></div>
-        <div className="game-cell"></div>        
-        <div className="game-cell"></div>
-        <div className="game-cell"></div>
-        <div className="game-cell"></div>
-        <div className="game-cell"></div>
-
-        <div className="game-cell"></div>
-        <div className="game-cell"></div>        
-        <div className="game-cell"></div>
-        <div className="game-cell"></div>        
-        <div className="game-cell"></div>
-        <div className="game-cell"></div>
-        <div className="game-cell"></div>
-        <div className="game-cell"></div>
-
+       }}>   
+          {[...Array(64)].map((e, i) => <div className="game-cell">{i}</div>)}
        </div>
     </div>
   );
